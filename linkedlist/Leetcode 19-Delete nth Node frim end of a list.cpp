e03b0f05729fabf2e9f452dee13836719e4c41cb
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2021-09-29 12:55:16 +0530

    Create Leetcode 19-Delete nth Node frim end of a list.cpp

---
+++ linkedlist/Leetcode 19-Delete nth Node frim end of a list.cpp
@@ -0,0 +1,74 @@
+//approach 1
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+        if(head==NULL)
+            return NULL;
+        
+        ListNode*curr=head;
+        
+        int count=0;
+        
+        while(curr!=NULL)
+        {
+            count+=1;
+            curr=curr->next;
+        }
+        
+        if(count==n)
+            return head->next;
+    
+         curr=head;
+        count=count-n;
+        
+        ListNode* prev=NULL;
+        
+        for(int i=0;i<count;i++){
+            prev=curr;
+            curr=curr->next;
+        }
+        
+        prev->next=curr->next;
+        
+        return head;
+    }
+};
+
+//approach 2 imp(optimal)
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+        if(head==NULL)
+            return NULL;
+        
+        ListNode*dummy=new ListNode(0);
+        dummy->next=head;
+        
+        ListNode *fast=dummy;
+        ListNode* slow=dummy;
+        
+        for(int i=1;i<=n+1;i++){
+            fast=fast->next;
+        }
+        
+        while(fast!=NULL){
+            fast=fast->next;
+            slow=slow->next;
+        }
+        slow->next=slow->next->next;
+        
+        return dummy->next;
+    }
+};
+
+
+
+
+
+
+
+
+
