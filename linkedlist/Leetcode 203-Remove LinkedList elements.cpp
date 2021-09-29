d59ae4da8cc5fc246eecc4d1d4e5716471ecc1b2
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2021-09-29 11:36:15 +0530

    Create Leetcode 203-Remove LinkedList elements.cpp

---
+++ linkedlist/Leetcode 203-Remove LinkedList elements.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        
+        if(head==NULL)
+            return NULL;
+        
+       while( head!=NULL && head->val==val ){
+           head=head->next;
+       } 
+        
+        
+       ListNode* curr=head;
+        
+        while(curr!=NULL && curr->next!=NULL) {
+            
+            if(curr->next->val==val){
+                curr->next=curr->next->next;
+            }
+            else
+            curr=curr->next;
+            
+        }
+        
+        return head;
+    }
+};
