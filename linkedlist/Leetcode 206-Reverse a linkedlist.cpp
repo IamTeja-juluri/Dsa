b27ef526e40228164e542be302db842c16d8dddc
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2021-09-29 10:58:22 +0530

    Create Leetcode 206-Reverse a linkedlist.cpp

---
+++ linkedlist/Leetcode 206-Reverse a linkedlist.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+  
+    ListNode* reverseList(ListNode* head) {
+     
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        while(curr!=NULL)
+        {
+            ListNode* temp=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+    }
+};
+
+
