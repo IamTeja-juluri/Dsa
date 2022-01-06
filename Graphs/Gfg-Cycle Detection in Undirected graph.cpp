54c0eb79037e906de560b239572b5d07c913d3eb
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-01-06 13:22:40 +0530

    Update Gfg-Cycle Detection in Undirected graph.cpp

--- Graphs/Gfg-Cycle Detection in Undirected graph.cpp
+++ Graphs/Gfg-Cycle Detection in Undirected graph.cpp
@@ -1,4 +1,81 @@
-//Using Bfs
+//Approach 1-Using dfs
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+   
+   bool dfs(int node,int parent,vector<int>adj[],vector<bool>&visited){
+       
+       visited[node]=true;
+       
+       for(auto nbr:adj[node]){
+           
+           if(!visited[nbr]){
+               
+               bool subprob=dfs(nbr,node,adj,visited);
+               
+               if(subprob){
+                   return true;
+               }
+               
+           }
+           else if(parent!=nbr){
+               return true;
+           }
+           
+       }
+      
+      return false;
+       
+   }
+    
+    
+    
+    bool isCycle(int v, vector<int> adj[]) {
+        // Code here
+        vector<bool>visited(v,false);
+        
+        for(int i=0;i<v;i++){
+         
+            if(!visited[i] && dfs(i,-1,adj,visited))
+             return true;
+         
+        }
+        
+        return false;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        bool ans = obj.isCycle(V, adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
+    }
+    return 0;
+}  // } Driver Code Ends
+
+
+//Approach 2-Using Bfs
 // { Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
