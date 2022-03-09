537086fd2694a52dfaa93bca5cf7d509b8bf61f8
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-03-09 19:05:23 +0530

    Create Leetcode-168. Excel Sheet Column Title.cpp

---
+++ String/Leetcode-168. Excel Sheet Column Title.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string convertToTitle(int n) {
+     
+        string ans="";
+        
+        while(n){
+            n--;
+            ans+='A'+(n)%26;
+            n/=26;
+        }
+        
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
