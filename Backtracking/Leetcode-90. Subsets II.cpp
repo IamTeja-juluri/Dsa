a73624d80a09870eedda02cafb260b9303b822ad
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-03-10 12:17:42 +0530

    Update Leetcode-90. Subsets II.cpp

--- Backtracking/Leetcode-90. Subsets II.cpp
+++ Backtracking/Leetcode-90. Subsets II.cpp
@@ -33,3 +33,40 @@ public:
         return res;
     }
 };
+//Better app -No need of set since we ve sorted array already
+class Solution {
+public:
+    
+    vector<vector<int> >res;
+    
+    void subsets(vector<int>&nums,vector<int>&v,int idx){
+        
+        if(idx==nums.size()){
+           res.push_back(v);
+            return;
+        }
+        
+        v.push_back(nums[idx]);
+        subsets(nums,v,idx+1);
+        v.pop_back();
+        
+        while(idx+1<nums.size() and nums[idx]==nums[idx+1]) idx++;
+        
+        subsets(nums,v,idx+1);
+    
+    }
+    
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+     
+        vector<int> v;
+        subsets(nums,v,0);
+
+    
+        return res;
+    }
+};
+
