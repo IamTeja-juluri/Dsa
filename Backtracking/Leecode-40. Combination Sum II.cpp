ccf35f626f2c914dd9f9bf8201fb553b1e64f863
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-03-10 12:19:32 +0530

    Update Leecode-40. Combination Sum II.cpp

--- Backtracking/Leecode-40. Combination Sum II.cpp
+++ Backtracking/Leecode-40. Combination Sum II.cpp
@@ -44,3 +44,41 @@ public:
         return res;
     }
 };
+//better app without set
+class Solution {
+public:
+    
+   vector<vector<int>>res;
+    
+    void combinations(vector<int>&nums,vector<int>&v,int target,int idx){
+        
+        if(target==0){
+            res.push_back(v);
+            return ;
+        }
+        
+        if(idx==nums.size())
+            return;
+            
+        if(nums[idx]<=target){
+            v.push_back(nums[idx]);
+            combinations(nums,v,target-nums[idx],idx+1);
+            v.pop_back();
+        }
+        
+        while(idx+1<nums.size() and nums[idx]==nums[idx+1]) idx++;
+        combinations(nums,v,target,idx+1);
+        
+    }
+    
+    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
+      
+      int n=nums.size(); 
+      sort(nums.begin(),nums.end());
+          
+      vector<int> v;
+      combinations(nums,v,target,0);
+        
+        return res;
+    }
+};
