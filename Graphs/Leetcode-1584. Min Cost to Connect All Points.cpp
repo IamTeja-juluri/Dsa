93ab509c5692f89774dad92118b7adc9096ac4d2
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-03-17 13:04:14 +0530

    Update Leetcode-1584. Min Cost to Connect All Points.cpp

--- Graphs/Leetcode-1584. Min Cost to Connect All Points.cpp
+++ Graphs/Leetcode-1584. Min Cost to Connect All Points.cpp
@@ -94,3 +94,79 @@ public:
         return kruskals(n);
     }
 };
+//prims algo
+class Solution {
+public:
+    
+    vector<pair<int,int> >*l;
+    
+  
+     int manhattan(vector<int>&b,vector<int>&a){
+        
+        int dist=abs(b[0]-a[0])+abs(b[1]-a[1]);
+        return dist;
+        
+    }
+    
+    
+    void addEdge(int x,int y,int wt){
+        l[x].push_back({y,wt});
+        l[y].push_back({x,wt});
+    }
+    
+    
+    int prims(int n){
+        
+        
+      priority_queue<pair<int,int>,vector<pair<int,int> >,greater<pair<int,int> > > pq;
+      
+      bool *visited=new bool[n]{false};  
+      int mincost=0;
+      
+      pq.push({0,0});
+        
+        while(!pq.empty()){
+            
+            auto curr=pq.top();
+            pq.pop();
+            
+            int dest=curr.second;
+            int weight=curr.first;
+            
+            if(visited[dest]==true)
+                continue;
+            
+            visited[dest]=true;
+            mincost+=weight;
+            
+            for(auto nbr:l[dest]){
+                if(visited[nbr.first]==false){
+                    pq.push({nbr.second,nbr.first});
+                }
+            }
+            
+            
+        }
+        
+        return mincost;
+    }
+    
+    
+   
+    
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        
+        int n=points.size();
+        
+        l=new vector<pair<int,int>>[n];  
+        
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                int manhattan_dist=manhattan(points[j],points[i]);
+                addEdge(i,j,manhattan_dist);
+            }
+        }
+    
+        return prims(n);
+    }
+};
