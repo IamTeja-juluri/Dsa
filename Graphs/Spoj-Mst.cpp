14c49ee16562c8536602b6029bbe449724143841
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-03-17 12:46:37 +0530

    Update Spoj-Mst.cpp

--- Graphs/Spoj-Mst.cpp
+++ Graphs/Spoj-Mst.cpp
@@ -1,3 +1,4 @@
+//kruskals algo
 #include<bits/stdc++.h>
 using namespace std;
 class DSU{
@@ -78,4 +79,80 @@ int main(){
     
     
      cout<<mincost<<endl;
+}
+//prims algo
+#include<bits/stdc++.h>
+using namespace std;
+
+class Graph{
+  
+  vector<pair<int,int>> *l;
+  int v;
+  
+  public:
+  
+  Graph(int n){
+      v=n;
+      l=new vector<pair<int,int>>[n];
+  }
+  
+  void addEdge(int x,int y,int w){
+      l[x].push_back({y,w});
+      l[y].push_back({x,w});
+  }
+  
+  
+  int prims_algo(){
+      
+      priority_queue<pair<int,int>,vector<pair<int,int> >,greater<pair<int,int>>>q;
+      
+      bool *visited=new bool[v]{false};
+      int ans=0;
+      
+      q.push({0,0});
+      
+      while(!q.empty()){
+          
+          auto curr=q.top();
+          q.pop();
+          
+          int dest=curr.second;
+          int weight=curr.first;
+          
+          if(visited[dest]==true)
+          continue;
+          
+          ans+=weight;
+          visited[dest]=true;
+          
+          for(auto x:l[dest]){
+              if(visited[x.first]==false){
+                  q.push({x.second,x.first});
+              }
+          }
+          
+      }
+      
+      return ans;
+      
+  }
+    
+    
+};
+
+int main(){
+    
+    int n,m;
+    cin>>n>>m;
+    
+    Graph g(n);
+    
+    for(int i=0;i<n;i++){
+        int x,y,w;
+        cin>>x>>y>>w;
+        g.addEdge(x-1,y-1,w);
+    }
+    
+    cout<<g.prims_algo()<<endl;
+    
 }
