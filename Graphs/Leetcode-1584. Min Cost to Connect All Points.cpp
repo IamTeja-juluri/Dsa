3e3bc1b4cbea2366dd1f36cf5e7dd8d14f662e5d
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-03-17 11:58:36 +0530

    Create Leetcode-1584. Min Cost to Connect All Points.cpp

---
+++ Graphs/Leetcode-1584. Min Cost to Connect All Points.cpp
@@ -0,0 +1,96 @@
+//kruskals algo
+class DSU{
+  public:
+    
+    int *parent;
+    int *rank;
+    
+    DSU(int v){
+        
+        parent=new int[v];
+        rank=new int[v];
+        
+        for(int i=0;i<v;i++){
+            parent[i]=-1;
+            rank[i]=1;
+        }
+    }
+    
+    int find(int i){
+        if(parent[i]==-1)
+            return i;
+        return parent[i]=find(parent[i]);
+    }
+    
+    void unite(int x,int y,int s1,int s2){
+    
+        
+            if(rank[s1]<rank[s2]){
+                rank[s2]+=rank[s1];
+                parent[s1]=s2;
+            }
+            else{
+                rank[s1]+=rank[s2];
+                parent[s2]=s1;
+            }
+  
+      }
+    
+};
+
+
+class Solution {
+public:
+    
+    //take pairs instead of vector as pairs are fast 
+    vector<pair<int,pair<int,int> > >edgelist;
+     
+    int manhattan(vector<int>&b,vector<int>&a){
+        
+        int dist=abs(b[0]-a[0])+abs(b[1]-a[1]);
+        return dist;
+        
+    }
+    
+
+      int kruskals(int n){
+          
+        DSU s(n);
+        
+        int mincost=0;
+        sort(edgelist.begin(),edgelist.end());
+        
+        for(auto edge:edgelist){
+            
+            int wt=edge.first;
+            int x=edge.second.first;
+            int y=edge.second.second;
+            
+            int s1=s.find(x);
+            int s2=s.find(y);
+            
+            if(s1!=s2){
+                mincost+=wt;
+                s.unite(x,y,s1,s2);
+            }
+            
+        } 
+        
+        return mincost;
+    }
+    
+    
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        
+        int n=points.size();
+        
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                int manhattan_dist=manhattan(points[j],points[i]);
+                edgelist.push_back({manhattan_dist,{i,j}});
+            }
+        }
+    
+        return kruskals(n);
+    }
+};
