f17c45787180b945f80b16248fd400c5920671ae
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-03-21 19:49:07 +0530

    Create Leetcode-1025. Divisor Game.cpp

---
+++ Game Theory/Leetcode-1025. Divisor Game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+        
+        //draw true or false table and find out reln
+        
+        if(n<=1 || (n%2)==1)
+            return false;
+        
+        return true;
+    }
+};
