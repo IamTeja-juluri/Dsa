2a0c63285c03d5cef6e093ca319d5ddc448926bd
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-03-22 12:35:43 +0530

    Update Leetcode-980. Unique Paths III.cpp

--- Backtracking/Leetcode-980. Unique Paths III.cpp
+++ Backtracking/Leetcode-980. Unique Paths III.cpp
@@ -1,23 +1,86 @@
 class Solution {
 public:
     
-//     int dx[4],dy[4];
+    int dx[4],dy[4];
     int ans;
     vector<vector<bool>>visited;
     
-//     Solution(){
+    Solution(){
         
-//         dx[0]=1;
-//         dx[1]=-1;
-//         dx[2]=0;
-//         dx[3]=0;
+        dx[0]=1;
+        dx[1]=-1;
+        dx[2]=0;
+        dx[3]=0;
         
-//         dy[0]=0;
-//         dy[1]=0;
-//         dy[2]=1;
-//         dy[3]=-1;
+        dy[0]=0;
+        dy[1]=0;
+        dy[2]=1;
+        dy[3]=-1;
       
-//     }
+    }
+    
+    void backTrack(vector<vector<int>>&grid,int x,int y,int c){
+        
+        if(grid[x][y]==2){
+            if(c==-1)
+               ans+=1;
+            return;
+        }
+        
+        visited[x][y]=true;
+        
+        for(int i=0;i<4;i++){
+            int newx=x+dx[i];
+            int newy=y+dy[i];
+            if(newx>=0 && newx<grid.size() && newy>=0 && newy<grid[0].size() && grid[newx][newy]!=-1 && visited[newx][newy]==false){
+                backTrack(grid,newx,newy,c-1);
+            }
+        }
+        
+        visited[x][y]=false;
+    
+    }
+    
+    
+    
+    int uniquePathsIII(vector<vector<int>>& grid) {
+        
+        int n=grid.size();
+        int m=grid[0].size();
+     
+        
+        int spx,spy;
+        int count0=0;
+        ans=0;
+        
+        visited.resize(n,vector<bool>(m,false));
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    spx=i;
+                    spy=j;
+                }
+                if(grid[i][j]==0)
+                    count0+=1;
+            }
+        }
+        
+        backTrack(grid,spx,spy,count0);
+        
+        return ans;
+    }
+};
+
+//same logic in diff way
+
+class Solution {
+public:
+   
+    
+    int ans;
+    vector<vector<bool>>visited;
+    
     
     void backTrack(vector<vector<int>>&grid,int x,int y,int c){
         
@@ -72,3 +135,5 @@ public:
         return ans;
     }
 };
+
+
