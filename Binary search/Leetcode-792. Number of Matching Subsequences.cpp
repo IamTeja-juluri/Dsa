97d15ce4066ade3b5e9c9b26b29c9fbd642a5d1d
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-03-24 10:42:16 +0530

    Update Leetcode-792. Number of Matching Subsequences.cpp

--- Binary search/Leetcode-792. Number of Matching Subsequences.cpp
+++ Binary search/Leetcode-792. Number of Matching Subsequences.cpp
@@ -1,3 +1,52 @@
+//Binary search
+class Solution {
+public:
+    int numMatchingSubseq(string s, vector<string>& words) {
+        
+        int n=s.size();
+        int m=words.size();
+        
+       unordered_map<char,vector<int>>mp;
+        
+        for(int i=0;i<n;i++)
+            mp[s[i]].push_back(i);
+        
+        int ans=0;
+       
+        
+        
+        for(auto x:words){
+            
+            bool found=true;
+            int previdx=-1;
+            
+            for(int i=0;x[i]!='\0';i++){
+                  
+                
+                if(mp.find(x[i])==mp.end()){
+                    found=false;
+                    break;
+                }
+                else{
+                    
+                    auto pos=upper_bound(mp[x[i]].begin(),mp[x[i]].end(),previdx);
+                    if(pos==mp[x[i]].end()){
+                        found=false;
+                        break;
+                    }
+                    else
+                        previdx=*pos;
+                }
+            }
+            
+            if(found)
+                ans+=1;
+        }
+        
+        return ans;
+    }
+};
+//linear search
 class Solution {
 public:
     int numMatchingSubseq(string s, vector<string>& words) {
