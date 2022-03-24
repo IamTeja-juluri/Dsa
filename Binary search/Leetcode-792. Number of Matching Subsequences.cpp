a93fbccdc087c7d7af6c4950fb0adb75eaff17e1
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-03-24 09:55:48 +0530

    Create Leetcode-792. Number of Matching Subsequences.cpp

---
+++ Binary search/Leetcode-792. Number of Matching Subsequences.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int numMatchingSubseq(string s, vector<string>& words) {
+        
+        int n=s.size();
+        int m=words.size();
+        
+       unordered_map<char,vector<int>>mp;
+        
+        for(int i=0;i<n;i++)
+            mp[s[i]].push_back(i);
+        
+        int ans=0;
+       
+        
+        
+        for(auto x:words){
+            
+            bool found=true;
+            int previdx=-1;
+            
+            for(int i=0;x[i]!='\0';i++){
+                
+                if(mp.find(x[i])==mp.end()){
+                    found=false;
+                    break;
+                }
+                else{
+                    int temp=previdx;
+                    for(auto idx:mp[x[i]]){
+                        if(idx>previdx){
+                            previdx=idx;
+                            break;
+                        }
+                    }
+                    if(temp==previdx)
+                        found=false;
+                }
+                
+            }
+            
+            
+            if(found)
+                ans+=1;
+        }
+        
+        return ans;
+    }
+};
