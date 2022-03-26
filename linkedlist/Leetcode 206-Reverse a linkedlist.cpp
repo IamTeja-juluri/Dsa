3229d35ecc10ad382ed3320c45561e0c723eaa21
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-03-26 15:40:45 +0530

    Update Leetcode 206-Reverse a linkedlist.cpp

--- linkedlist/Leetcode 206-Reverse a linkedlist.cpp
+++ linkedlist/Leetcode 206-Reverse a linkedlist.cpp
@@ -37,4 +37,49 @@ public:
     }
 };
 
+//Java 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+        
+//         if(head==null)
+//             return null;
+        
+//         if(head.next==null)
+//             return head;
+        
+//         ListNode curr=reverseList(head.next);
+        
+//         head.next.next=head;
+//         head.next=null;
+        
+//         return curr;
+        
+      //  Iterative
+//         if(head==null)
+//             return null;
+        
+//         ListNode prev=null,curr=head;
+        
+//         while(curr!=null){
+//             ListNode temp=curr.next;
+//             curr.next=prev;
+//             prev=curr;
+//             curr=temp;
+//         }
+        
+//         return prev;
+        
+//     }
+//}
+
 
