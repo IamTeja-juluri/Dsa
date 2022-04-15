9b7959a20668936e7404a3d947551f40e452840f
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-04-15 18:55:20 +0530

    Create Leetcode-1514. Path with Maximum Probability.cpp

---
+++ Graphs/Leetcode-1514. Path with Maximum Probability.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int src, int dest) {
+        
+        vector<double> dist(n+1,0.0);
+        list<pair<int,double> > *l;
+        l=new list<pair<int,double>>[n];
+        
+       for(int i=0;i<edges.size();i++){
+           l[edges[i][0]].push_back({edges[i][1],succProb[i]});
+           l[edges[i][1]].push_back({edges[i][0],succProb[i]});
+       }
+       
+        
+        priority_queue<pair<double,int> >pq;
+        
+        dist[src]=0.0;
+        pq.push({1.0,src});
+        
+        while(!pq.empty()){
+            
+            int node=pq.top().second;
+            double nodewt=pq.top().first;
+            pq.pop();
+            
+            for(auto nbrPair:l[node]){
+                
+                double nbrwt=nbrPair.second;
+                int nbr=nbrPair.first;
+                
+                if(dist[nbr]<(nodewt*nbrwt)){
+                    dist[nbr]=nodewt*nbrwt;
+                    pq.push({dist[nbr],nbr});
+                }
+            
+            }
+            
+            
+        }
+        
+        return dist[dest];
+    }
+};
