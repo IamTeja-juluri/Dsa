35def85dd14905146b23edb8aaaedb18089d5725
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-05-03 12:07:26 +0530

    Create Leetcode-923. 3Sum With Multiplicity.cpp

---
+++ Arrays/Leetcode-923. 3Sum With Multiplicity.cpp
@@ -0,0 +1,54 @@
+//Soln using two ptrs
+
+class Solution {
+public:
+    int threeSumMulti(vector<int>& a, int target) {
+        
+        long long count=0;
+        int n=a.size();
+        sort(a.begin(),a.end());
+        int mod=1e9+7;
+        
+        for(int i=0;i<(n-2);i++){
+            
+            int targetsum=target-a[i];
+            int p1=i+1;
+            int p2=n-1;
+            
+            while(p1<p2){
+                
+                if((a[p1]+a[p2])<targetsum)
+                    p1++;
+                else if((a[p1]+a[p2])>targetsum)
+                    p2--;
+                
+                else{
+                    
+                    long long l=1,r=1;
+                    while(p1<p2 && a[p1]==a[p1+1]){
+                        p1++;
+                        l++;
+                    }
+                     while(p1<p2 && a[p2]==a[p2-1]){
+                        p2--;
+                        r++;
+                    }
+                    
+                    if(p1==p2)
+                        count=(count+(l*(l-1))/2)%mod;
+                    else
+                        count=(count+(l*r))%mod;
+                    
+                    p1++;
+                    p2--;
+                }
+                
+            }
+            
+        }
+        
+        return count;
+        
+        
+    }
+};
