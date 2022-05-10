e5f117464ef7d8975a1ca655de4301800893f33a
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-05-10 19:06:45 +0530

    Update Leetcode-189. Rotate Array.cpp

--- Arrays/Leetcode-189. Rotate Array.cpp
+++ Arrays/Leetcode-189. Rotate Array.cpp
@@ -1,3 +1,4 @@
+//app1 with extra space
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
@@ -30,6 +31,38 @@ public:
             nums[i]=x;
             i+=1;
         }
+        
+    }
+};
+//app2 without extra space
+//reversal technique
+class Solution {
+public:
+    
+    
+    void reverse(vector<int>&nums,int l,int h){
+        
+        while(l<=h){
+           swap(nums[l],nums[h]);
+            l+=1;
+            h-=1;
+        }
+        
+        return;
+    }
+    
+    
+    
+    void rotate(vector<int>& nums, int k) {
+        
+        int n=nums.size();
+         
+        k=k%n;
+        
+        reverse(nums,0,n-1); //reverse d whole array
+        reverse(nums,0,k-1); //reverse first k numbers
+        reverse(nums,k,n-1); //reverse last n-k elements
+        
         
     }
 };
