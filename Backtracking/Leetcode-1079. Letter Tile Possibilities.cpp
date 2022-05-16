05c1775d76780909717e70ab4b08c645affecc6f
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-05-16 09:29:30 +0530

    Create Leetcode-1079. Letter Tile Possibilities.cpp

---
+++ Backtracking/Leetcode-1079. Letter Tile Possibilities.cpp
@@ -0,0 +1,56 @@
+//Not optimal but gives ac
+class Solution {
+public:
+    
+    vector<string> v;
+    unordered_set<string> st;
+    
+    void permutations(string &s,int l,int r){
+        
+        if(l==r){
+            v.push_back(s);
+            return;
+        }
+        
+        for(int i=l;i<=r;i++){
+            
+            swap(s[i],s[l]);
+            permutations(s,l+1,r);
+            swap(s[i],s[l]);
+        }
+        
+        return;
+    }
+    
+    
+    void generateSubsets(string &s,string &ans,int idx,int n){
+        
+        if(idx==n){
+            st.insert(ans);
+            return;
+        }
+        
+        ans.push_back(s[idx]);
+        generateSubsets(s,ans,idx+1,n);
+        ans.pop_back();
+        generateSubsets(s,ans,idx+1,n);
+        
+    }
+    
+    
+    int numTilePossibilities(string s) {
+        
+        int n=s.size();
+        
+        permutations(s,0,n-1);
+        
+        for(auto x:v){
+            int n1=x.size();
+            string temp="";
+            generateSubsets(x,temp,0,n1);
+        }
+        
+        int cnt=st.size();
+        return cnt-1;
+    }
+};
