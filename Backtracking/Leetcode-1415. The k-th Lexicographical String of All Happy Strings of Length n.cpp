6b2ce173bf074214a98ff536f5551b01ca263069
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-05-16 11:24:59 +0530

    Update Leetcode-1415. The k-th Lexicographical String of All Happy Strings of Length n.cpp

--- Backtracking/Leetcode-1415. The k-th Lexicographical String of All Happy Strings of Length n.cpp
+++ Backtracking/Leetcode-1415. The k-th Lexicographical String of All Happy Strings of Length n.cpp
@@ -1,4 +1,42 @@
-class Solution {
+1)class Solution {
+public:
+    
+   vector<string> ans;
+    
+    void constructStrings(string &s,int idx,int n){
+        
+        if(n==0){
+            ans.push_back(s);
+            return;
+        }
+        
+       for(char ch='a';ch<='c';ch++){
+           if(s.size()==0 || s.back()!=ch){
+              s.push_back(ch);
+              constructStrings(s,idx+1,n-1);
+               s.pop_back();
+           }
+       }
+        
+        return ;
+    }
+    
+    
+    string getHappyString(int n, int k) {
+        
+        string temp="";
+        constructStrings(temp,0,n);
+       
+        int size=ans.size();
+        
+        if(k<=size)
+             return ans[k-1];
+        
+        return "";
+
+    }
+};
+2)class Solution {
 public:
     
    vector<string> ans;
