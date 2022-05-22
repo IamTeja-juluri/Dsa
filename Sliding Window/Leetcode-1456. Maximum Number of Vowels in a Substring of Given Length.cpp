05146e38a9d7ee06f2a0b8a918a66bc33eef07c9
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-05-22 17:32:05 +0530

    Update Leetcode-1456. Maximum Number of Vowels in a Substring of Given Length.cpp

--- Sliding Window/Leetcode-1456. Maximum Number of Vowels in a Substring of Given Length.cpp
+++ Sliding Window/Leetcode-1456. Maximum Number of Vowels in a Substring of Given Length.cpp
@@ -25,3 +25,40 @@ public:
         return ans;
     }
 };
+
+//java
+class Solution {
+    public int maxVowels(String s, int k) {
+     
+         int ans=0;
+        int len=0;
+        
+    
+        
+        for(int i=0;i<k;i++){
+            
+            char ch=s.charAt(i);
+            
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+                len+=1;
+        }
+        
+        ans=len;
+       
+        for(int i=0,j=k;j<s.length();i++,j++){
+            
+            char ch1=s.charAt(i);
+            char ch2=s.charAt(j);
+            if(ch1=='a' || ch1=='e' || ch1=='i' || ch1=='o' || ch1=='u')
+                len-=1;
+             if(ch2=='a' || ch2=='e' || ch2=='i' || ch2=='o' || ch2=='u')
+                len+=1;
+             ans=Math.max(ans,len);    
+           
+        }
+        
+        return ans;
+        
+        
+    }
+}
