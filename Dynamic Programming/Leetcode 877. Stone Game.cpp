dd30e3844a56305c1dc393978982b16bb0cf2dd0
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-06-03 17:53:13 +0530

    Update Leetcode 877. Stone Game.cpp

--- Dynamic Programming/Leetcode 877. Stone Game.cpp
+++ Dynamic Programming/Leetcode 877. Stone Game.cpp
@@ -1,3 +1,4 @@
+//app1
 class Solution {
 public:
      vector<vector<int>> dp;
@@ -33,5 +34,37 @@ public:
         return p1score>p2score;
         
         
+    }
+};
+
+//app2
+class Solution {
+public:
+    int dp[501][501];
+    
+   int solve(vector<int>&piles,int l,int r)
+   {
+     if(l==r-1) return max(piles[l],piles[r]);
+     
+     if(dp[l][r]!=-1) return dp[l][r];
+     
+     int leftchoice =piles[l] + max(solve(piles,l+2,r),solve(piles,l+1,r-1));
+     int rightchoice=piles[r] + max(solve(piles,l,r-2),solve(piles,l+1,r-1));
+     
+     return dp[l][r]=max(leftchoice,rightchoice);
+   }
+    bool stoneGame(vector<int>& piles) {
+     memset(dp,-1,sizeof(dp));
+     int r=piles.size()-1;
+     int alice=solve(piles,0,r);
+     
+      int sum=0;
+      for(int i=0;i<piles.size();i++)
+      {
+        sum+=piles[i];
+      }
+      
+      return alice>(sum-alice);
+      
     }
 };
