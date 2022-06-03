ac3cac8470c9d5cc523e44fada1b3653c60a6396
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-06-03 18:55:49 +0530

    Update Leetcode-1690. Stone Game VII.java

--- Dynamic Programming/Leetcode-1690. Stone Game VII.java
+++ Dynamic Programming/Leetcode-1690. Stone Game VII.java
@@ -1,4 +1,5 @@
-//standard template
+//standard template 
+//app1
 class Solution {
     
     int computeDifference(int[] stones,int[] pref,int[][] dp,int i,int j){
@@ -33,6 +34,45 @@ class Solution {
                 dp[i][j]=-1;
         
        return computeDifference(stones,pref,dp,0,n-1);
+        
+        
+    }
+}
+//app2
+//without prefix arr
+//remember we dont need sum dimension for dp array-reason unknown ,it just works in quesns like dis
+class Solution {
+    
+    int computeDifference(int[] stones,int sum,int[][] dp,int i,int j){
+        
+        if(i>j)
+            return 0;
+        
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        
+        int op1=sum-stones[i]-computeDifference(stones,sum-stones[i],dp,i+1,j);
+        int op2=sum-stones[j]-computeDifference(stones,sum-stones[j],dp,i,j-1);
+        
+        return dp[i][j]=Math.max(op1,op2);
+        
+    }
+    
+    
+    public int stoneGameVII(int[] stones) {
+        
+        int n=stones.length;
+        int[][] dp=new int[n][n];
+        int sum=0;
+      
+        for(int i=0;i<n;i++)
+            sum+=stones[i];
+        
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+                dp[i][j]=-1;
+        
+       return computeDifference(stones,sum,dp,0,n-1);
         
         
     }
