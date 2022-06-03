546d00fed02e91640760fc1091eb9f32bf47914b
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-06-03 18:45:14 +0530

    Update Leetcode-1406. Stone Game III.cpp

--- Dynamic Programming/Leetcode-1406. Stone Game III.cpp
+++ Dynamic Programming/Leetcode-1406. Stone Game III.cpp
@@ -1,3 +1,4 @@
+//app1
 class Solution {
 public:
     
@@ -47,5 +48,73 @@ public:
             ans="Bob";
         
         return ans;
+    }
+};
+//app2
+class Solution {
+public:
+    int dp[50000+10][2];
+    int knapsack(vector<int>&arr,int i,int j,
+                 int chance)
+    {
+        if(i>j)
+        {
+            return 0;
+        }
+        if(dp[i][chance]!=-1)
+            return dp[i][chance];
+        if(chance==0)
+        {
+            int x=INT_MIN,y=INT_MIN,z=INT_MIN;
+            if(i<arr.size())
+            {
+                x=arr[i]+knapsack(arr,i+1,j,1);
+            }
+            if(i+1<arr.size())
+            {
+                y=arr[i]+arr[i+1]+knapsack(arr,i+2,j,1);
+            }
+            if(i+2<arr.size())
+            {
+                z=arr[i]+arr[i+1]+arr[i+2]+knapsack(arr,i+3,j,1);
+            }
+          
+            return dp[i][chance]= max(max(x,y),z);
+        }
+        if(chance==1)
+        {
+            int x=INT_MAX,y=INT_MAX,z=INT_MAX;
+            if(i<arr.size())
+            {
+                x=knapsack(arr,i+1,j,0);
+            }
+            if(i+1<arr.size())
+            {
+                y=knapsack(arr,i+2,j,0);
+            }
+            if(i+2<arr.size())
+            {
+                z=knapsack(arr,i+3,j,0);
+            }
+            return dp[i][chance]= min(min(x,y),z);
+            
+        }
+        return 0;
+    }
+    string stoneGameIII(vector<int>& stoneValue) {
+        memset(dp,-1,sizeof dp);
+        int sum=0;
+        for(auto i : stoneValue)
+        {
+            sum+=i;
+        }
+        int x=knapsack(stoneValue,0,stoneValue.size()-1,0);
+        
+        if(sum-2*x<0)
+            return "Alice";
+        if(sum-2*x==0)
+            return "Tie";
+        return "Bob";
+        
     }
 };
