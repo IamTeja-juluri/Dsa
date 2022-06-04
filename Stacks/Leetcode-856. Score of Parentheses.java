cd7734d841e2a5b6725b6afe8238235f57e8c764
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-06-04 11:45:02 +0530

    Create Leetcode-856. Score of Parentheses.java

---
+++ Stacks/Leetcode-856. Score of Parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int scoreOfParentheses(String s) {
+               
+        int n=s.length();
+        
+       Stack<Integer> st=new Stack<>();
+        
+        int score=0;
+        
+        for(char ch:s.toCharArray()){
+            
+            if(ch=='('){
+                st.push(score);
+                score=0;
+            }
+            else
+                score=st.pop()+Math.max(2*score,1);
+        }
+        
+        return score;
+    }
+}
