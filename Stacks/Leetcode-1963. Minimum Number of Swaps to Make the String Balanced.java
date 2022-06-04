a665575e2eafecfba33f5accc3e4786bf93c7435
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-06-04 12:21:25 +0530

    Update Leetcode-1963. Minimum Number of Swaps to Make the String Balanced.java

--- Stacks/Leetcode-1963. Minimum Number of Swaps to Make the String Balanced.java
+++ Stacks/Leetcode-1963. Minimum Number of Swaps to Make the String Balanced.java
@@ -1,6 +1,15 @@
 class Solution {
     public int minSwaps(String s) {
         
+        //algo 
+        //we assign 1 to right paranthesis
+        //we assign -1 to left paranthesis
+        //at any point in time maximum value of score
+        //is the max swaps we can make to make string balanced
+        // but if we look carefully one swap not just makes one pair balanced
+        // but if makes two pairs balanced 
+        //so for minimum swaps we take half of max swaps
+        
         int score=0;
         int ans=Integer.MIN_VALUE;
         for(char ch:s.toCharArray()){
