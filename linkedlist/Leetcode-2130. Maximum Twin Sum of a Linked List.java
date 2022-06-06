1935ecc3cbb4c7db34245fed683bab551f3133a3
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-06-06 20:55:54 +0530

    Create Leetcode-2130. Maximum Twin Sum of a Linked List.java

---
+++ linkedlist/Leetcode-2130. Maximum Twin Sum of a Linked List.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    
+    //idea is to reverse second half of list
+    //or we can use stack
+    
+    ListNode reverse(ListNode head){
+        
+        ListNode curr=head;
+        ListNode prev=null;
+        
+        while(curr!=null){
+            ListNode temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        
+        return prev;
+    }
+    
+    public int pairSum(ListNode head) {
+        
+        ListNode slow=head;
+        ListNode fast=head.next;
+        
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        
+        ListNode curr2=reverse(slow.next);
+        
+        int ans=Integer.MIN_VALUE;
+        ListNode curr1=head;
+        
+        while(curr2!=null){
+            ans=Math.max(ans,curr1.val+curr2.val);
+            curr1=curr1.next;
+            curr2=curr2.next;
+        }
+        
+        return ans;
+    }
+}
