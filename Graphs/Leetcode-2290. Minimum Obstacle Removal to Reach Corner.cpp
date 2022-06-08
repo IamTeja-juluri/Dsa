da6dd0f6238203cb9102f4a58366c651163291c3
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-06-08 11:06:46 +0530

    Update Leetcode-2290. Minimum Obstacle Removal to Reach Corner.cpp

--- Graphs/Leetcode-2290. Minimum Obstacle Removal to Reach Corner.cpp
+++ Graphs/Leetcode-2290. Minimum Obstacle Removal to Reach Corner.cpp
@@ -1,6 +1,6 @@
+//app1  -Using Bfs simple
 class Solution {
 public:
-    // remember dis approach
     int minimumObstacles(vector<vector<int>>& grid) {
         
         queue<pair<int,int> >q;
@@ -33,6 +33,53 @@ public:
                     }
                 }
             }
+        }
+        
+        return dist[n-1][m-1];
+    }
+};
+ // Using dijkshtra approach
+class Solution {
+public:
+    int minimumObstacles(vector<vector<int>>& grid) {
+        
+        queue<pair<int,int> >q;
+        
+        int dx[]={1,-1,0,0};
+        int dy[]={0,0,1,-1};
+        
+        int n=grid.size();
+        int m=grid[0].size();
+        
+        vector<vector<int> > dist(n,vector<int>(m,INT_MAX));
+        
+        set<pair<int,pair<int,int> > >st;
+        st.insert({0,{0,0}});
+        
+        while(!st.empty()){
+            
+            auto it=st.begin();
+            int currDist=it->first;
+            int x=it->second.first;
+            int y=it->second.second;
+            st.erase(it);
+            
+            for(int i=0;i<4;i++){
+                int newx=x+dx[i];
+                int newy=y+dy[i];
+                if(newx>=0 && newx<n && newy>=0 && newy<m){
+                    int wt=0;
+                    if(grid[newx][newy]==1)
+                        wt=1;
+                    if(currDist+wt<dist[newx][newy]){
+                        if(st.find({dist[newx][newy],{newx,newy}})!=st.end())
+                            st.erase({dist[newx][newy],{newx,newy}});
+                        dist[newx][newy]=currDist+wt;
+                        st.insert({dist[newx][newy],{newx,newy}});
+                    }
+                }
+            }
+            
         }
         
         return dist[n-1][m-1];
