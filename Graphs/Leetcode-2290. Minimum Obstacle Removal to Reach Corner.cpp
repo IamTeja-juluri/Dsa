ea9d9e1b9c6b5f4aece8d31549ada54743e98d01
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-06-08 11:13:40 +0530

    Update Leetcode-2290. Minimum Obstacle Removal to Reach Corner.cpp

--- Graphs/Leetcode-2290. Minimum Obstacle Removal to Reach Corner.cpp
+++ Graphs/Leetcode-2290. Minimum Obstacle Removal to Reach Corner.cpp
@@ -38,7 +38,7 @@ public:
         return dist[n-1][m-1];
     }
 };
- // Using dijkshtra approach
+ // Using dijkshtra approach using Set
 class Solution {
 public:
     int minimumObstacles(vector<vector<int>>& grid) {
@@ -76,6 +76,52 @@ public:
                             st.erase({dist[newx][newy],{newx,newy}});
                         dist[newx][newy]=currDist+wt;
                         st.insert({dist[newx][newy],{newx,newy}});
+                    }
+                }
+            }
+            
+        }
+        
+        return dist[n-1][m-1];
+    }
+};
+//  dijkshtaras approach using priorityQueue
+class Solution {
+public:
+   
+    int minimumObstacles(vector<vector<int>>& grid) {
+        
+     
+        int dx[]={1,-1,0,0};
+        int dy[]={0,0,1,-1};
+        
+        int n=grid.size();
+        int m=grid[0].size();
+        
+        vector<vector<int> > dist(n,vector<int>(m,INT_MAX));
+        
+        priority_queue<pair<int,pair<int,int> >,vector<pair<int,pair<int,int> > >,greater<pair<int,pair<int,int> > > > pq;
+        pq.push({0,{0,0}});
+        
+        while(!pq.empty()){
+            
+            // pair<int,pair<int,int> > p=pq.top();
+            auto p=pq.top();
+            int currDist=p.first;
+            int x=p.second.first;
+            int y=p.second.second;
+            pq.pop();
+            
+            for(int i=0;i<4;i++){
+                int newx=x+dx[i];
+                int newy=y+dy[i];
+                if(newx>=0 && newx<n && newy>=0 && newy<m){
+                    int wt=0;
+                    if(grid[newx][newy]==1)
+                        wt=1;
+                    if(currDist+wt<dist[newx][newy]){
+                        dist[newx][newy]=currDist+wt;
+                        pq.push({dist[newx][newy],{newx,newy}}); //we allow duplictes instead of searching a pair of pair in pq because the dist gets larger for every itr
                     }
                 }
             }
