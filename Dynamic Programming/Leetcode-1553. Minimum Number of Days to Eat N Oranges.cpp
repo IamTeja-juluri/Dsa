db9569368de413c932c535bf68d9bcab16a827c9
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-06-10 14:55:19 +0530

    Create Leetcode-1553. Minimum Number of Days to Eat N Oranges.cpp

---
+++ Dynamic Programming/Leetcode-1553. Minimum Number of Days to Eat N Oranges.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    map<int,int> mp; // using maps to avoid tle
+    
+    int helper(int n){
+        
+        if(n==0)
+            return 0;
+        
+        if(n==1)
+            return 1;
+        
+        if(mp.find(n)!=mp.end())
+            return mp[n];
+        
+        // int op1=1+helper(n-1); gives runtime error cause it takes n number of calls
+        // for op2=n becomes n-n/2 = n/2;
+        // for op3=n becomes n-2n/3 = n/3;
+        //now the prob reduces to making n divisible by 2 or 3
+        
+        return mp[n]=1+min( n%2 + helper(n/2) , n%3 + helper(n/3) );
+    }
+    
+    
+    int minDays(int n) {
+        
+        return helper(n);
+    }
+};
