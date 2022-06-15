554cdbbc95ed80b043ca74cc783ad3083b003c2e
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-06-15 19:42:06 +0530

    Create Leetcode-1284. Minimum Number of Flips to Convert Binary Matrix to Zero Matrix.cpp

---
+++ Graphs/Leetcode-1284. Minimum Number of Flips to Convert Binary Matrix to Zero Matrix.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    
+    vector<vector<int> > flipMatrix(vector<vector<int> > mat,int x,int y){
+        
+        int n=mat.size();
+        int m=mat[0].size();
+        
+        mat[x][y]^=1;
+        
+        if(x-1>=0)
+            mat[x-1][y]^=1;
+        
+         if(x+1<n)
+            mat[x+1][y]^=1;
+        
+         if(y-1>=0)
+            mat[x][y-1]^=1;
+        
+        if(y+1<m)
+            mat[x][y+1]^=1;
+        
+       return mat;
+        
+    }
+    
+    
+    int minFlips(vector<vector<int>>& mat) {
+        
+        int n=mat.size();
+        int m=mat[0].size();
+        
+        vector<vector<int> > res(n,vector<int>(m,0));
+        
+        set<vector<vector<int> > > visited;
+        
+        queue<vector<vector<int> > > q;
+        int steps=0;
+        q.push(mat);
+        
+        while(!q.empty()){
+            
+            int n=q.size();
+            
+            for(int i=1;i<=n;i++){
+                
+                auto v=q.front();
+                q.pop();
+                
+                if(v==res)
+                    return steps;
+                
+                if(visited.find(v)!=visited.end())
+                continue;
+                
+                visited.insert(v);
+                
+                for(int j=0;j<mat.size();j++){
+                    for(int k=0;k<mat[0].size();k++){
+                        vector<vector<int> > r=flipMatrix(v,j,k);
+                        q.push(r);
+                    }
+                } 
+                
+            }
+            
+            steps+=1;
+        }
+        
+        
+        return -1;   
+    }
+};
