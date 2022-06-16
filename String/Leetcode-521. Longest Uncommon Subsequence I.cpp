2c4a95c1e4761cef3b56ca202f1337fb991c2ec7
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-06-16 23:34:03 +0530

    Create Leetcode-521. Longest Uncommon Subsequence I.cpp

---
+++ String/Leetcode-521. Longest Uncommon Subsequence I.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findLUSlength(string a, string b) {
+    
+        int n1=a.size();
+        int n2=b.size();
+        
+         if(a==b)
+             return -1;
+        
+        return max(n1,n2);
+    }
+};
