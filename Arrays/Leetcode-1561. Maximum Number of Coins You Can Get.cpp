f4fe83f0b05ce0b7b374b3db1816e8fe69796da1
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-06-18 08:46:33 +0530

    Update Leetcode-1561. Maximum Number of Coins You Can Get.cpp

--- Arrays/Leetcode-1561. Maximum Number of Coins You Can Get.cpp
+++ Arrays/Leetcode-1561. Maximum Number of Coins You Can Get.cpp
@@ -1,3 +1,4 @@
+//soln1
 class Solution {
 public:
     int maxCoins(vector<int>& piles) {
@@ -11,6 +12,33 @@ public:
             if(i%2)
                 ans+=piles[i];
         }
+        
+        return ans;
+    }
+};
+//soln2-using heap
+class Solution {
+public:
+    int maxCoins(vector<int>& piles) {
+        
+        int n=piles.size();
+        int ans=0;
+        
+        priority_queue<int> maxhp;
+           
+        for(auto x:piles)
+            maxhp.push(x);
+        
+        int i=0;
+          
+       while(!maxhp.empty() && i<(2*n)/3){
+           
+           maxhp.pop();
+           ans+=maxhp.top();
+           maxhp.pop();
+           i+=2;
+       }
+        
         
         return ans;
     }
