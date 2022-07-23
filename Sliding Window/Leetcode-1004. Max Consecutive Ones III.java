51f55cccc6b9d186f07bbb52df9690c2ba3b8a9d
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-07-23 11:19:40 +0530

    Update Leetcode-1004. Max Consecutive Ones III.java

--- Sliding Window/Leetcode-1004. Max Consecutive Ones III.java
+++ Sliding Window/Leetcode-1004. Max Consecutive Ones III.java
@@ -1,3 +1,32 @@
+//cpp
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+     
+        
+        int n=nums.size();
+        int zeroes=0;
+        int ans=0;
+        int j=0;
+        
+        for(int i=0;i<n;i++){
+            if(nums[i]==0)
+                zeroes+=1;
+            while(zeroes>k){
+                if(nums[j]==0)
+                    zeroes-=1;
+                j+=1;
+            }
+            ans=max(i-j+1,ans);
+        }
+        
+        
+        
+        return ans;
+    }
+};
+
+//java
 class Solution {
     public int longestOnes(int[] nums, int k) {
         
