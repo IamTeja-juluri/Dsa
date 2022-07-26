b44b8bda826c518ed538dbba03cde23232f5ca58
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-07-26 19:37:08 +0530

    Update Leetcode-1839. Longest Substring Of All Vowels in Order.cpp

--- Sliding Window/Leetcode-1839. Longest Substring Of All Vowels in Order.cpp
+++ Sliding Window/Leetcode-1839. Longest Substring Of All Vowels in Order.cpp
@@ -14,3 +14,26 @@ public:
         return ans;
     }
 };
+//app2
+class Solution {
+public:
+    int longestBeautifulSubstring(string word) {
+        
+         int ans = 0;
+        unordered_map<char,int>mp;
+        mp[word[0]]+=1;
+        
+        int j=0;
+        for(int i=1;i<word.size();i++){
+            if(word[i]<word[i-1]){
+                j=i;
+                mp.clear();
+            }
+            mp[word[i]]+=1;
+            if(mp.size()==5)
+                ans=max(ans,i-j+1);
+            
+        }
+        return ans;
+    }
+};
