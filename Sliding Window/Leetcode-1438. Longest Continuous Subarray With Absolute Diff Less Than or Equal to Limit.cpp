507c7116930e07b096b8ed6e97c75ac47d8b9058
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-07-29 17:30:35 +0530

    Update Leetcode-1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp

--- Sliding Window/Leetcode-1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp
+++ Sliding Window/Leetcode-1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp
@@ -27,3 +27,46 @@ public:
         return ans;
     }
 };
+
+//using heap
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        
+        int j=0;
+        
+       int n=nums.size();
+       int ans=0;
+       multiset<int>m;
+        
+       priority_queue<pair<int,int> > maxhp;
+       priority_queue<pair<int,int>,vector<pair<int,int> >,greater<pair<int,int> > > minhp;
+        
+    
+        for(int i=0;i<n;i++){
+            
+            maxhp.push({nums[i],i});
+            minhp.push({nums[i],i});
+            
+            while(maxhp.top().first-minhp.top().first>limit){
+                
+                int pos=min(maxhp.top().second,minhp.top().second);
+                if(maxhp.top().second==pos)
+                    maxhp.pop();
+                else
+                    minhp.pop();
+            
+                while(maxhp.top().second<pos)
+                    maxhp.pop();
+                while(minhp.top().second<pos)
+                    minhp.pop();
+                j=pos+1;
+            }
+            
+            ans=max(ans,i-j+1);
+            
+        }
+        
+        return ans;
+    }
+};
