978fa456073c0a746fa1b5c3fdd4dc6f8226e3c9
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-08-20 17:57:04 +0530

    Update Leetcode-450. Delete Node in a BST.cpp

--- BinaryTrees/Leetcode-450. Delete Node in a BST.cpp
+++ BinaryTrees/Leetcode-450. Delete Node in a BST.cpp
@@ -15,6 +15,46 @@
 class Solution {
 public:
     
+    TreeNode* findMin(TreeNode* root){
+        while(root->left)
+            root=root->left;
+        return root;
+    }
+    
+ 
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        
+      if(root==NULL)
+          return NULL;
+        
+      if(root->val>key)
+          root->left=deleteNode(root->left,key);
+     else if(root->val<key)
+          root->right=deleteNode(root->right,key);
+     else{
+         if(!root->left and !root->right){
+           delete root;
+           root=NULL;  
+         }
+         else if(root->left==NULL)
+             *root=*(root->right);
+         else if(root->right==NULL)
+             *root=*(root->left);
+         else{
+             TreeNode* curr=findMin(root->right);
+             root->val=curr->val;
+             root->right=deleteNode(root->right,curr->val);
+         }
+     }   
+        
+        return root;
+    }
+};
+
+
+class Solution {
+public:
+    
     int findPredecessor(TreeNode* root){
         root=root->left;
         while(root->right)
@@ -55,20 +95,7 @@ public:
     }
 };
 
-//app2
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-
-
+//app3
 class Solution {
 public:
     
