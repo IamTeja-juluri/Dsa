728fca7a6cc00847382dd6ed68ef9beeee8c15a8
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-08-20 17:35:55 +0530

    Create Leetcode-450. Delete Node in a BST.cpp

---
+++ BinaryTrees/Leetcode-450. Delete Node in a BST.cpp
@@ -0,0 +1,56 @@
+//app1
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+
+class Solution {
+public:
+    
+    int findPredecessor(TreeNode* root){
+        root=root->left;
+        while(root->right)
+            root=root->right;
+        return root->val;
+    } 
+    
+    int findSuccessor(TreeNode* root){
+        root=root->right;
+        while(root->left)
+            root=root->left;
+        return root->val;
+    }
+ 
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        
+      if(root==NULL)
+          return NULL;
+        
+      if(root->val>key)
+          root->left=deleteNode(root->left,key);
+     else if(root->val<key)
+          root->right=deleteNode(root->right,key);
+     else{
+         if(!root->left and !root->right)
+            root=nullptr;
+         else if(root->left){
+             root->val=findPredecessor(root);
+             root->left=deleteNode(root->left,root->val);
+         }
+         else{
+              root->val=findSuccessor(root);
+              root->right=deleteNode(root->right,root->val);
+         }
+     }   
+        
+        return root;
+    }
+};
