842bcaea38793d9ae4fd402e1ae446f6eb93e49b
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-08-26 11:31:38 +0530

    Update Leetcode 15. 3Sum.cpp

--- Binary search/Leetcode 15. 3Sum.cpp
+++ Binary search/Leetcode 15. 3Sum.cpp
@@ -1,3 +1,42 @@
+//app1
+class Solution {
+public:
+    
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+        int n=nums.size();
+        vector<vector<int> > res;
+        
+        sort(nums.begin(),nums.end());
+        
+        for(int i=0;i<n;i++){
+            int left=i+1;
+            int right=n-1;
+            int reqsum=-nums[i];
+            while(left<right){
+                if(nums[left]+nums[right]>reqsum)
+                    right-=1;
+                else if(nums[left]+nums[right]<reqsum)
+                    left+=1;
+                else{
+                    int secondvalue=nums[left];
+                    int thirdvalue=nums[right];
+                    res.push_back({nums[i],secondvalue,thirdvalue});
+                    while(left<right and nums[left]==secondvalue)
+                        left+=1;
+                    while(right>left and nums[right]==thirdvalue)
+                        right-=1;
+                }
+            }
+            while(i+1<n and nums[i]==nums[i+1])
+                i+=1;
+        }
+        
+     
+        return res;
+    }
+};
+//app2
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& a) {
