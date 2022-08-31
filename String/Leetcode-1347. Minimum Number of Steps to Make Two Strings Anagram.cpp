21be5b513265c21a3487fb5861480782dab1f9a8
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-08-31 12:37:10 +0530

    Update Leetcode-1347. Minimum Number of Steps to Make Two Strings Anagram.cpp

--- String/Leetcode-1347. Minimum Number of Steps to Make Two Strings Anagram.cpp
+++ String/Leetcode-1347. Minimum Number of Steps to Make Two Strings Anagram.cpp
@@ -1,3 +1,26 @@
+//app1
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        
+        int count[26]={0};
+        
+        for(auto x:s)
+            count[x-'a']+=1;
+        
+        for(auto x:t)
+            count[x-'a']-=1;
+        
+        int cnt=0;
+        
+        for(int i=0;i<26;i++)
+            cnt+=abs(count[i]);
+        
+        return cnt/2;
+    }
+};
+
+//app2
 class Solution {
 public:
     int minSteps(string s, string t) {
