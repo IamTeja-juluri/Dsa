bd58a846f8e151feb7689aa033bdf31e5e8ba9ed
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-09-21 10:05:47 +0530

    Update Leetcode-152. Maximum Product Subarray.cpp

--- Dynamic Programming/Leetcode-152. Maximum Product Subarray.cpp
+++ Dynamic Programming/Leetcode-152. Maximum Product Subarray.cpp
@@ -28,3 +28,28 @@ public:
         
     }
 };
+//gfg
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        
+        int n=nums.size();
+        
+        if(n==1)
+            return nums[0];
+        
+        int prod=1;
+        int maxprod=INT_MIN;
+        int min_eh=0;
+        int max_eh=0;
+        
+        for(int i=0;i<n;i++){
+            int temp=max({max_eh*nums[i],min_eh*nums[i],nums[i]});
+            min_eh=min({min_eh*nums[i],nums[i],max_eh*nums[i]});
+            max_eh=temp;
+            maxprod=max(maxprod,max_eh);
+        }
+     
+        return maxprod;
+        
+    }
