aa17aac8cbebb521b6ba2e822b1082090b0a4334
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-09-21 09:51:51 +0530

    Create Leetcode-152. Maximum Product Subarray.cpp

---
+++ Dynamic Programming/Leetcode-152. Maximum Product Subarray.cpp
@@ -0,0 +1,30 @@
+//refer ayushi vermas video
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        
+        int n=nums.size();
+        
+        int prod=1;
+        int maxprod=INT_MIN;
+        
+        for(int i=0;i<n;i++){
+            prod=prod*nums[i];
+            maxprod=max(maxprod,prod);
+            if(prod==0)
+                prod=1;
+        }
+        
+        prod=1;
+        
+        for(int i=n-1;i>=0;i--){
+            prod=prod*nums[i];
+            maxprod=max(maxprod,prod);
+            if(prod==0)
+                prod=1;
+        }
+        
+        return  maxprod;
+        
+    }
+};
