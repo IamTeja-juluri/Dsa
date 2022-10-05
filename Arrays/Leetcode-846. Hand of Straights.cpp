a7657c81ae6814d91d0d923dbd7e107dffcc8e14
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-10-05 17:58:01 +0530

    Create Leetcode-846. Hand of Straights.cpp

---
+++ Arrays/Leetcode-846. Hand of Straights.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        
+        map<int,int> mp;
+        int n=hand.size();
+        
+        for(auto x:hand)
+            mp[x]+=1;
+        
+        int no_of_groups=n/groupSize;
+        
+        int groups=0;
+        
+        while(mp.size()!=0 ){
+            
+            auto it=mp.begin();
+            int num=it->first;
+            int size=0;
+            
+             while(mp.find(num)!=mp.end() and size<groupSize){
+                mp[num]-=1;
+                if(mp[num]==0)
+                    mp.erase(num);
+                num+=1;
+                size+=1;
+            }
+            
+            if(size==groupSize)
+                groups+=1;
+            else
+                return false;
+        }
+        
+        
+        return groups==no_of_groups;
+        
+    }
+};
