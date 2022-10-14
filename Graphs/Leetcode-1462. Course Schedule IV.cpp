a8aeaa5a3f9b1eb3d910371d793cb4dbcc303554
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-10-14 14:39:34 +0530

    Update Leetcode-1462. Course Schedule IV.cpp

--- Graphs/Leetcode-1462. Course Schedule IV.cpp
+++ Graphs/Leetcode-1462. Course Schedule IV.cpp
@@ -48,3 +48,57 @@ public:
         return ans;
     }
 };
+
+//using bfs-toposort
+class Solution {
+public:
+    
+    vector<vector<bool> > isReachable;
+    vector<vector<int> > adj;
+    
+  
+    vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& pre, vector<vector<int>>& queries) {
+        
+        vector<int> indegree(n,0);
+        queue<int> q;
+        vector<bool> ans;
+        adj.resize(n,{});
+        isReachable.resize(n,vector<bool>(n,false));
+        
+        for(auto x:pre){
+            adj[x[0]].push_back(x[1]);
+            indegree[x[1]]+=1;
+            isReachable[x[0]][x[1]]=true;
+        }
+        
+        for(int i=0;i<n;i++)
+            if(indegree[i]==0)
+                q.push(i);
+        
+        
+        while(!q.empty()){
+            
+            int node=q.front();
+            q.pop();
+            
+            for(auto nbr:adj[node]){
+                
+                for(int i=0;i<n;i++){
+                    if(isReachable[i][node])
+                        isReachable[i][nbr]=true;
+                }
+                
+                indegree[nbr]-=1;
+                if(indegree[nbr]==0)
+                    q.push(nbr);
+            }
+            
+        }
+        
+        for(auto x:queries)
+            ans.push_back(isReachable[x[0]][x[1]]);
+        
+       return ans;
+        
+    }
+};
