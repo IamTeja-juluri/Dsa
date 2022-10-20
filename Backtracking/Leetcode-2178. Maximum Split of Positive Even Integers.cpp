680e7b6ada74de4888b6f0fcc4fcb9ce971271a1
IamTeja-juluri <85792253+IamTeja-juluri@users.noreply.github.com>
2022-10-20 18:51:46 +0530

    Create Leetcode-2178. Maximum Split of Positive Even Integers.cpp

---
+++ Backtracking/Leetcode-2178. Maximum Split of Positive Even Integers.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    set<long long>st;
+    
+    vector<long long> helper(long long curr_sum,long long finalSum){
+        
+        if(curr_sum==finalSum){
+            vector<long long> ans;
+            for(auto x:st)
+                ans.push_back(x);
+            return ans;
+        }
+        
+        for(long long i=2;i<=finalSum;i+=2){
+            if(st.find(i)!=st.end() or curr_sum+i>finalSum)
+                continue;
+            st.insert(i);
+            vector<long long> res=helper(curr_sum+i,finalSum);
+            if(res.size()>0)
+                return res;
+            st.erase(i);
+        }
+        
+        return {};
+    }
+    
+    vector<long long> maximumEvenSplit(long long finalSum) {
+        
+        if(finalSum%2==1)
+            return {};
+        return helper(0,finalSum);  
+        
+        
+    }
+};
